7d8009e50b7ac2c5cb7b1787220e84adf62282cd
shauf <shauf@gmail.com>
2025-02-02 21:49:03 +0200

    Add function for Azure Storage

---
+++ PtgExpressHub.StorageFunction/ArtifactsUploadFunction.cs
@@ -0,0 +1,16 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace PtgExpressHub.StorageFunction;
+
+public class ArtifactsUploadFunction
+{
+    [FunctionName("on-artifacts-uploaded-function")]
+    public static void Run2([BlobTrigger("comport-application-blob/{name}")] Stream myBlob, string name, ILogger log)
+    {
+        log.LogInformation("C# BlobTrigger function processed a request.");
+
+        log.LogInformation($"Blob Triggered: {name}");
+        log.LogInformation($"Blob Size: {myBlob.Length} bytes");
+    }
+}
